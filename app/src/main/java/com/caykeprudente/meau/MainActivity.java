039d1024ad2ea2a5fbf799bf8f2086939d0dc24a
Cayke Prudente <cayke10@gmail.com>
2016-11-03 01:32:59 -0200

    -tela inicial

--- app/src/main/java/com/caykeprudente/meau/MainActivity.java
+++ app/src/main/java/com/caykeprudente/meau/MainActivity.java
@@ -1,13 +1,43 @@
 package com.caykeprudente.meau;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_adopt, btn_register, btn_help;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_adopt = (Button) findViewById(R.id.activity_main_btn_adopt);
+        btn_help = (Button) findViewById(R.id.activity_main_btn_help);
+        btn_register = (Button) findViewById(R.id.activity_main_btn_register);
+
+
+        btn_adopt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //todo
+            }
+        });
+
+        btn_help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //todo
+            }
+        });
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //todo
+            }
+        });
     }
 }
