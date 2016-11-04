974c8c2b3d304248ca4ac880f6f84ffff3d7eef2
Cayke Prudente <cayke10@gmail.com>
2016-11-04 13:15:17 +0000

    resolving conflits

--- app/src/main/java/com/caykeprudente/meau/MainActivity.java
+++ app/src/main/java/com/caykeprudente/meau/MainActivity.java
@@ -1,14 +1,18 @@
 package com.caykeprudente.meau;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button btn_adopt, btn_register, btn_help;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,24 +27,35 @@ public class MainActivity extends AppCompatActivity {
         btn_adopt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //todo
+                Intent intent = new Intent(MainActivity.this, AppActivity.class);
+                intent.putExtra("initial_screen", getString(R.string.adopt_pet));
+                startActivity(intent);
+                finish();
             }
         });
 
         btn_help.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //todo
+                Intent intent = new Intent(MainActivity.this, AppActivity.class);
+                intent.putExtra("initial_screen", getString(R.string.help_pet));
+                startActivity(intent);
+                finish();
             }
         });
 
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //login
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                Intent intent = new Intent(MainActivity.this, AppActivity.class);
+                intent.putExtra("initial_screen", getString(R.string.register_pet));
                 startActivity(intent);
+                finish();
             }
         });
+
+        Spannable text = new SpannableString(getSupportActionBar().getTitle());
+        text.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.greenPool)), 0, text.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+        getSupportActionBar().setTitle(text);
     }
 }
